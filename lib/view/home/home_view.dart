580ff0f289dfbf05184be157f0be7140f8274fb0
TanerTkn <tanertekin2025@gmail.com>
2021-04-14 15:44:12 +0300

    todaybiggest eklendi

--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -1,9 +1,13 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_musics/constant/color_constant.dart';
 import 'package:google_musics/constant/svg_icons.dart';
+import 'package:google_musics/core/services/playlists.dart';
+import 'package:google_musics/models/playlist.dart';
 import 'package:google_musics/widgets/appBar.dart';
 import 'package:google_musics/core/extension/context_extension.dart';
+import 'package:google_musics/widgets/today_biggest_hits.dart';
 
 class HomeView extends StatefulWidget {
   @override
@@ -13,18 +17,48 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   ColorConstants constants = ColorConstants.instance;
   SvgIcons svgIcons = SvgIcons.instance;
+  PlaylistService service = PlaylistService();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: CustomAppBar(
-      backgroundColor: Theme.of(context).primaryColor,
-      elevation: 1.0,
-      text: "Listen Now",
-      actions: [
-        CupertinoButton(child: svgIcons.broadcast, onPressed: () {}),
-        CupertinoButton(child: svgIcons.search, onPressed: () {})
-      ],
-      height: context.sizeH(0.08),
-    ));
+      body: Container(
+        child: SingleChildScrollView(
+            child: Column(
+          children: [
+            SizedBox(
+              height: context.sizeH(0.01),
+            ),
+            Text("It's Friday Night", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 25)),
+            SizedBox(
+              height: context.sizeH(0.005),
+            ),
+            Text(
+              "Play something for...",
+              style: TextStyle(color: Colors.black),
+            ),
+            SizedBox(
+              height: context.sizeH(0.005),
+            ),
+            FutureBuilder(
+              future: service.getPlaylists(),
+              builder: (context, AsyncSnapshot<List<Playlist>> snapshot) {
+                return CarouselSlider(
+                    items: snapshot.data.map((data) {
+                      return TodayBiggestHits(
+                        playlist: data,
+                      );
+                    }).toList(),
+                    options: CarouselOptions());
+              },
+            )
+          ],
+        )),
+      ),
+      appBar: CustomAppBar(
+        text: "Listen Now",
+        actions: [CupertinoButton(child: svgIcons.broadcast, onPressed: () {}), CupertinoButton(child: svgIcons.search, onPressed: () {})],
+        height: context.sizeH(0.08),
+      ),
+    );
   }
 }
--- lib/widgets/today_biggest_hits.dart
+++ lib/widgets/today_biggest_hits.dart
@@ -1,13 +1,31 @@
-
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:google_musics/core/extension/context_extension.dart';
+import 'package:google_musics/models/playlist.dart';
 
 class TodayBiggestHits extends StatelessWidget {
-  const TodayBiggestHits({Key key}) : super(key: key);
+  final Playlist playlist;
+  const TodayBiggestHits({Key key, this.playlist}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(6),
+      ),
+      child: Column(
+        children: [
+          Image.asset(playlist.image),
+          SizedBox(
+            height: 15,
+          ),
+          Text(
+            playlist.name,
+            style: TextStyle(color: Colors.black, fontWeight: FontWeight.w600, fontSize: 20),
+          )
+        ],
+      ),
     );
   }
 }
