24b2e97895bef0d84e6de5a7119b53cb2b9a7c04
TanerTkn <tanertekin2025@gmail.com>
2021-04-14 03:30:41 +0300

    files upgrade

---
+++ lib/constant/color_constant.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class ColorConstants {
+  static ColorConstants instance = ColorConstants._init();
+
+  ColorConstants._init();
+
+  final roseOfSharon = Color(0xFFC45900);
+  final dodgerBlue = Color(0xFF536DFE);
+}
---
+++ lib/core/base/base_state.dart
@@ -0,0 +1,5 @@
+import 'package:google_musics/constant/color_constant.dart';
+
+abstract class BaseState {
+  ColorConstants colorConstants = ColorConstants.instance;
+}
