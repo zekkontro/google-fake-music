aabcc45fab79c6ef452a80084380ac5b9eb6ff2f
zekkontro <brokbro052@gmail.com>
2021-04-14 11:47:23 +0300

    Base Service dosyası güncellendi

--- lib/core/services/base_service.dart
+++ lib/core/services/base_service.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart' show rootBundle;
 
-abstract class BaseService {
+abstract class BaseService extends ChangeNotifier {
   String apiPath = "assets/api.json";
   Future<String> getApiData() async {
     return await rootBundle.loadString(apiPath);
